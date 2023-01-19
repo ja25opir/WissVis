852c47298a13f440043fb6421402ff066985af3b
Nico <ps42begu@studserv.uni-leipzig.de>
2023-01-19 16:57:56 +0100

    Add function to find interesting cells

--- hauptaufgabe_src/mainplugin/RidgesAndValleys.cpp
+++ hauptaufgabe_src/mainplugin/RidgesAndValleys.cpp
@@ -40,7 +40,7 @@ namespace
         {
         }
 
-        bool findInterestingCell(const ValueArray<Point2>& gridPoints, Cell& cell, const ValueArray<Scalar>& fieldValues, std::shared_ptr<const Field<2, Scalar>> field) //
+        bool isInterestingCell(const ValueArray<Point2>& gridPoints, Cell& cell, const ValueArray<Scalar>& fieldValues, std::shared_ptr<const Field<2, Scalar>> field)
         {
             double epsilon = 0.1;
             std::vector<double> gradientsX;
@@ -50,36 +50,36 @@ namespace
 
             for(size_t i = 0; i < cell.numVertices(); ++i)
             {
+                //TODO Randbetrachtung
                 Point2 evaluatorPointX = gridPoints[cell.index(i)];
                 Point2 evaluatorPointY = gridPoints[cell.index(i)];
+
                 evaluatorPointX[0] += epsilon; //first in x direction
-                evaluatorPointY[1] += epsilon; //then in y direction
 
                 if(evaluator->reset(evaluatorPointX, 0))
                 {
                     auto valueX = evaluator->value();
                     gradientsX.push_back((valueX[0] - fieldValues[cell.index(i)][0]) / epsilon);
 
-                    infoLog() << "grid point X: " << evaluatorPointX << std::endl;
-                    infoLog() << "eval value: " << valueX[0] << std::endl;
-                    infoLog() << "gradient: " << gradientsX.back() << std::endl;
-
+                    //infoLog() << "grid point X: " << evaluatorPointX << std::endl;
+                    //infoLog() << "eval value: " << valueX[0] << std::endl;
+                    //infoLog() << "gradient: " << gradientsX.back() << std::endl;
                 }
                 else
                 {
                     infoLog() << "outside domain" << std::endl;
                 }
+
+                evaluatorPointY[1] += epsilon; //then in y direction
 
                 if(evaluator->reset(evaluatorPointY, 0))
                 {
                     auto valueY = evaluator->value();
                     gradientsY.push_back((valueY[0] - fieldValues[cell.index(i)][0]) / epsilon);
 
-                    infoLog() << "grid point Y: " << evaluatorPointY << std::endl;
-                    infoLog() << "eval value: " << valueY[0] << std::endl;
-                    infoLog() << "gradient: " << gradientsY.back() << std::endl;
-
-
+                    //infoLog() << "grid point Y: " << evaluatorPointY << std::endl;
+                    //infoLog() << "eval value: " << valueY[0] << std::endl;
+                    //infoLog() << "gradient: " << gradientsY.back() << std::endl;
                 }
                 else
                 {
@@ -87,29 +87,20 @@ namespace
                 }
             }
 
-            if(signbit(gradientsX[0]) != signbit(gradientsX[3]) || signbit(gradientsX[1]) != signbit(gradientsX[2]))
+            /*  Quad
+               0----3
+               |    |
+               |    |
+               1----2
+            */
+
+            if(signbit(gradientsX[0]) != signbit(gradientsX[3]) || signbit(gradientsX[1]) != signbit(gradientsX[2]) || signbit(gradientsY[0]) != signbit(gradientsY[1]) || signbit(gradientsY[2]) != signbit(gradientsY[3]))
             {
-                infoLog() << "------------------found interesting cell! "<< std::endl; //<< gradientsX[0] << ", " << gradientsX[3] << "," << gradientsX[1] << ", " << gradientsX[2] <<std::endl;
                 return true;
             }
             return false;
-
-                /*
-                auto value = evaluator2->value(cell.index(i));
-                infoLog() << "discrete value: " << value << std::endl;
-                infoLog() << "cell coord " << i << ": " << cell.index(i) << std::endl;
-                infoLog() << "grid point: " << gridPoints[cell.index(i)] << std::endl;
-                */
-
-                //infoLog() << gradient1 << "   " << gradient2 << std::endl;
-
-                /*
-                if(signbit(gradient1) != signbit(gradient2))
-                {
-                    infoLog() << "found interesting cell" << std::endl;
-                    return true;
-                }*/
         }
+
 
         virtual void execute( const Algorithm::Options& options, const volatile bool& /*abortFlag*/ ) override
         {
@@ -147,8 +138,9 @@ namespace
                 for(size_t i = 0; i < pGrid2D->numCells(); ++i)
                 {
                     Cell cell = pGrid2D->cell(i);
-                    if(findInterestingCell(pGridPoints2D, cell, pFieldValues2D, pField2D))
+                    if(isInterestingCell(pGridPoints2D, cell, pFieldValues2D, pField2D))
                     {
+                        infoLog() << "------------------found interesting cell at: " << i << std::endl;
                         interestingCell.push_back(cell);
                     }
                 }
