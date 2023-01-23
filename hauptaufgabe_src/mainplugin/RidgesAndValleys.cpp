782473a19a49bbfecaf67488b39876b12d71ac38
Marvin M <ja25opir@studserv.uni-leipzig.de>
2023-01-23 15:52:13 +0100

    add getCellCenter()

--- hauptaufgabe_src/mainplugin/RidgesAndValleys.cpp
+++ hauptaufgabe_src/mainplugin/RidgesAndValleys.cpp
@@ -107,8 +107,6 @@ namespace
 
         bool isInterestingCell(const ValueArray<Point2>& gridPoints, Cell& cell, const ValueArray<Scalar>& fieldValues, std::shared_ptr<const Field<2, Scalar>> field)
         {
-            double epsilon = 0.1;
-
             std::valarray<double> gradientX;
             std::valarray<double> gradientY;
             std::valarray<double> baseVectorX = {1,0};
@@ -124,64 +122,6 @@ namespace
                 gradientX = getPartialGradient(gridPoints, cell, i, fieldValues, evaluator, baseVectorX);
                 gradientY = getPartialGradient(gridPoints, cell, i, fieldValues, evaluator, baseVectorY);
 
-                //TODO Randbetrachtung
-                /*Point2 evaluatorPointX = gridPoints[cell.index(i)];
-                Point2 evaluatorPointY = evaluatorPointX;
-
-                evaluatorPointX[0] += epsilon; //first in x direction
-
-                if(evaluator->reset(evaluatorPointX, 0))
-                {
-                    auto valueX = evaluator->value();
-                    gradientX = ((valueX[0] - fieldValues[cell.index(i)][0]) / epsilon) * baseVectorX;
-                    //gradientsX.push_back((valueX[0] - fieldValues[cell.index(i)][0]) / epsilon);
-
-                    //infoLog() << "grid point X: " << evaluatorPointX << std::endl;
-                    //infoLog() << "eval value: " << valueX[0] << std::endl;
-                    //infoLog() << "gradient: " << gradientsX.back() << std::endl;
-                }
-                else
-                {
-                    evaluatorPointX[0] -= 2*epsilon;
-
-                    if(evaluator->reset(evaluatorPointX, 0))
-                    {
-                        auto valueX = evaluator->value();
-                        gradientX = ((fieldValues[cell.index(i)][0] - valueX[0]) / epsilon) * baseVectorX;
-                    }
-                    else
-                    {
-                        infoLog() << "outside domain" << std::endl;
-                    }
-                }
-
-                evaluatorPointY[1] += epsilon; //then in y direction
-
-                if(evaluator->reset(evaluatorPointY, 0))
-                {
-                    auto valueY = evaluator->value();
-                    gradientY = ((valueY[0] - fieldValues[cell.index(i)][0]) / epsilon) * baseVectorY;
-                    //gradientsY.push_back((valueY[0] - fieldValues[cell.index(i)][0]) / epsilon);
-
-                    //infoLog() << "grid point Y: " << evaluatorPointY << std::endl;
-                    //infoLog() << "eval value: " << valueY[0] << std::endl;
-                    //infoLog() << "gradient: " << gradientsY.back() << std::endl;
-                }
-                else
-                {
-                    evaluatorPointY[1] -= 2*epsilon;
-
-                    if(evaluator->reset(evaluatorPointY, 0))
-                    {
-                        auto valueY = evaluator->value();
-                        gradientY = ((fieldValues[cell.index(i)][0] - valueY[0]) / epsilon) * baseVectorY;
-                    }
-                    else
-                    {
-                        infoLog() << "outside domain" << std::endl;
-                    }
-                }**/
-
                 gradientCombined = gradientX + gradientY;
                 gradientVector.push_back(gradientCombined);
                 //infoLog() << "gradient: " << gradientCombined[0] << "; " << gradientCombined[1] << std::endl;
@@ -196,6 +136,17 @@ namespace
                 infoLog() << "empty list" << std::endl;
                 return false;
             }
+        }
+
+        Point2 getCellCenter2D(const ValueArray<Point2>& gridPoints, Cell& cell){
+            double sumX = 0;
+            double sumY = 0;
+            for(size_t i = 0; i < cell.numVertices(); ++i)
+            {
+                sumX += gridPoints[cell.index(i)][0];
+                sumY += gridPoints[cell.index(i)][1];
+            }
+            return {sumX / 4, sumY / 4};
         }
 
         bool isExtrema()
@@ -239,7 +190,6 @@ namespace
                 std::vector<int> interestingCellsIndices;
                 std::vector<int> extremaCellsIndices;
 
-
                 for(size_t i = 0; i < pGrid2D->numCells(); ++i)
                 {
                     Cell cell = pGrid2D->cell(i);
@@ -250,8 +200,8 @@ namespace
                         interestingCellsIndices.push_back(i);
                     }
                 }
-                infoLog() << "finished" << std::endl;
-                infoLog() << interestingCells.size() << std::endl; //5485
+                infoLog() << "interesting cells found: ";
+                infoLog() << interestingCells.size() << std::endl;
 
                 /*
                 for(size_t j = 0; j < interestingCellsIndices.size(); ++j)
